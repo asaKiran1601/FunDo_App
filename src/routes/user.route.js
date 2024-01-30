10de10476f94b99d0501dbf41c0b08c64e872bbd
asakiran1607 <asakiran55@gmail.com>
2024-01-30 10:06:07 +0530

    [AsaKiran]:Added Signup signin API

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,28 +5,12 @@ import * as userController from '../controllers/user.controller';
 
 const router = express.Router();
 
-// Sign-up API -------------------------
 
 //route to create a new signup user
 router.post('/signup', userController.signup);
 
-// Sign-In API ------------------------
 
 //route to create a new signin user
 router.post('/signin', userController.signin);
-
-
-// ----------------------
-// //route to create a new user
-// router.post('', newUserValidator, userController.newUser);
-
-// //route to get a single user by their user id
-// router.get('/email', userController.getUser);
-
-// //route to update a single user by their user id
-// router.put('/:_id', userController.updateUser);
-
-// //route to delete a single user by their user id
-// router.delete('/:_id', userController.deleteUser);
 
 export default router;
